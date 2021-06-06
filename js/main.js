8f894e6cb0e1f4023b755380755e2f48980abd39
Sergey <redmoloch@yanadex.ru>
2021-06-06 19:00:50 +0300

    added getRandomInt

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+const getRandomInt = (min, max) => {
+  [min, max] = [
+    Math.min(Math.abs(min), Math.abs(max)),
+    Math.max(Math.abs(min), Math.abs(max)),
+  ];
+  return Math.round(Math.random() * (max - min) + min);
+}
+
 // Функция для проверки максимальной длины строки
 
 const checkLength = (string, maxLength) => string.length <= maxLength;
 
-checkLength('qwsadcsca', 20);
+console.log(checkLength('qwsadcsca', 20), getRandomInt(1,3));
