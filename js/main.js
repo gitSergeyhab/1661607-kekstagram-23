1c2238b347cede7ef60cacc3e87a29eff04c11a3
Sergey <redmoloch@yandex.ru>
2021-06-20 08:32:18 +0300

    added removing eventListener on close modal

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 import {createPictureFragment} from './modules/create-picture-fragment.js';
 import {showFullScreenImg} from './modules/show-full-screen-img.js';
-import './modules/close-modal.js';
+import {hangCloseHandler} from './modules/close-modal.js';
 
 const picture = document.querySelector('.pictures');
 picture.append(createPictureFragment());
 
 picture.addEventListener('click', (evt) => {
   evt.preventDefault();
-  const needTarget = evt.target.closest('a');
+  const needTarget = evt.target.closest('a.picture');
   if (needTarget) {
     showFullScreenImg(needTarget.dataset.id);
     document.body.classList.add('modal-open');
+    hangCloseHandler();
   }
 });
--- js/modules/close-modal.js
+++ js/modules/close-modal.js
@@ -1,13 +1,26 @@
-const closeModal = () => {
+const btnCloseModal = document.querySelector('.big-picture__cancel');
+const onModalClose = () => {
   document.body.classList.remove('modal-open');
   document.querySelector('.big-picture').classList.add('hidden');
 };
 
-const btnCloseModal = document.querySelector('.big-picture__cancel');
-btnCloseModal.addEventListener('click', closeModal);
+const removeCloseHandler = () => {
+  btnCloseModal.removeEventListener('click', onModalClose);
+  document.removeEventListener('keydown', onModalClose);
+};
 
-document.addEventListener('keydown', (evt) => {
-  if (evt.keyCode === 27 && document.body.classList.contains('modal-open')) {
-    closeModal();
-  }
-});
+const hangCloseHandler = () => {
+  btnCloseModal.addEventListener('click', () => {
+    onModalClose();
+    removeCloseHandler();
+  } );
+  document.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 27 && document.body.classList.contains('modal-open')) {
+      onModalClose();
+      removeCloseHandler();
+    }
+  });
+};
+
+export {hangCloseHandler};
+
